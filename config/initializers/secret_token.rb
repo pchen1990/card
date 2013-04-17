3fc05e0e8ece27e3724148cc2834e2a010694f93
Phillip Chen <pchenster@gmail.com>
2013-04-16 18:20:52 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Card::Application.config.secret_token = 'd570c1e427bc30bb15c10260a02bf57c0c1948716610f8256177ba5b8fd4c3dacc22aa981c0c250b2eae478e8cedc53dd95d598c84211c27ea814cebfc9b50c1'
